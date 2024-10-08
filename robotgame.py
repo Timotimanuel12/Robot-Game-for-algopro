7a881e016c32d86cbc2cc68f52543005d59fe93a
Timotimanuel12 <timothy.imannuel@binus.ac.id>
2024-10-08 21:03:25 +0700

    Add files via upload

---
+++ robotgame.py
@@ -0,0 +1,62 @@
+import random
+Zones = {
+    "town": ['power cells', 'enemy', 'batteries'],
+    "village": ['batteries', 'power cells', 'enemy'],
+    "city": ['batteries', 'enemy', 'power cells']
+}
+
+
+Inventory = []
+power_cells = 0
+default_battery = 100
+current_zone = "Space"
+total_power_cells = 3
+
+def move_zone_to_zone(zone): #Moving from zone to zone
+    global current_zone
+    while zone not in Zones:
+        print("Zone is invalid, Please enter a valid zone! ")
+        zone = input("Please enter another zone (town, city or village): ")
+    print(f'Moving from {current_zone} to {zone}')
+    current_zone = zone
+    item = random.choice(Zones[zone])
+    print(f'You are in {zone} and have acquired some/encountered an {item}')
+    return item
+            
+
+def collect_item(item): # What happens when you collect the items
+    global power_cells, default_battery
+    if item == 'power cells':
+        Inventory.append(item)
+        print("Yay you have acquired a power cell! Collect all 3 to win")
+    elif item == 'batteries':
+        default_battery += 10
+        print("Yay! You have acquired batteries! 10% would be added to your current battery level.")
+    elif item == 'enemy':
+        default_battery -= 10
+        print("You have encountered an enemy! You managed to defeat them, but 10% is drained from your current battery level")
+    else:
+        print("There are no items to be added :(")
+
+def display_inventory(): #Displays the player's current inventory as a list
+    print(f'Inventory = {Inventory}')
+
+
+#Game Loop
+while default_battery > 0 and power_cells < total_power_cells:
+    print(f'Battery Percentage = {default_battery}')
+    print("Collect all 3 Power Cells to win the game!")
+    zone = input("Please enter a zone (town, village or city): ")
+    items = move_zone_to_zone(zone)
+    collected = collect_item(items)
+    display_inventory()
+
+    if len(Inventory) == total_power_cells: #Finish The game if all power cells are collected
+        print("Yay!! Congratulations! You have finished the game!!")
+        break
+    elif default_battery == 0: #finish the game if battery runs out
+        print("Oh no! You ran out of battery :( Game over.")
+        break
+
+
+
